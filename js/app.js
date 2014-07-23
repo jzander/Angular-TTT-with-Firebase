3c9179cc84b014afbaade78e447dc20531e2cf31
jzander <jeremy.zander@gmail.com>
2014-07-23 12:23:08 -0700

    pseudo code added to js file

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ TTTApp.controller('TTTController', function ($scope, $firebase) {
 
   $scope.testString = "Angular source, App, and Controller present" ;
 
-  $scope.cellList = [
+  $scope.cellList = [ // each individual cell with a unique ID.
 		{status: "A", id:0}, 
 		{status: "B", id:1}, 
 		{status: "C", id:2}, 
@@ -25,7 +25,7 @@ TTTApp.controller('TTTController', function ($scope, $firebase) {
 		{status: "I", id:8}
 	]  ;
 
-var winOptions = [
+var winOptions = [ // all possible winning combinations.
 	  [0,1,2],
 	  [3,4,5],
 	  [6,7,8],
@@ -36,47 +36,50 @@ var winOptions = [
 	  [2,5,8]
   ];
   
-$scope.p1 = [];
+$scope.p1 = []; //giving each player an empty array to store moves.
 $scope.p2 = [];
 
-  $scope.testJS = function() {
-    console.log('Correctly accessing JS function.') ;
+  $scope.testJS = function() { 
+    console.log('Correctly accessing JS function.') ; 
   } ;
 
  
-  $scope.playerPicks = function(thisCell) {
+  $scope.playerPicks = function(thisCell) { 
   		if ( thisCell.status == "X" || thisCell.status == "O" )
-  	  		return;	
+  	  		return;	 //if a cell has a status of X or Y, exit the function and render the box unclickable and unchangeable until next game.
   		  			
-		if ($scope.clickCount %2!=0){ 
+		if ($scope.clickCount %2!=0){ //every even move is O's turn
 			thisCell.status = "O" ;
-			thisCell.player = "p2";
-			$scope.p2.push(thisCell.id);
+			thisCell.player = "p2"; // O is player two
+			$scope.p2.push(thisCell.id); // put the unique ID number for the chosen cell into p2's array.
 			console.log(thisCell.status, thisCell.id, thisCell.player, $scope.p1, $scope.p2);
 		}
 			
 		else {
-			thisCell.status = "X" ;
-			thisCell.player = "p1";
-			$scope.p1.push(thisCell.id);
+			thisCell.status = "X" ; //all other moves are X's.
+			thisCell.player = "p1"; // X is player one.
+			$scope.p1.push(thisCell.id); // put the unique ID number for the chosen cell into p1's array.
 			console.log(thisCell.status, thisCell.id, thisCell.player, $scope.p1, $scope.p2);
 		}
 		
-		$scope.winCheck();
+		$scope.winCheck(); //run the wincheck function after each turn.
 			
 		$scope.clickCount = $scope.clickCount + 1 ;    
 		$scope.clickCounter.$set({clickCounter: $scope.clickCount}) ;
 	} ;
 
-		$scope.winCheck = function() {
-			for (var i = 0; i < winOptions.length; i++) {
+		$scope.winCheck = function() { //this is the win logic
+			for (var i = 0; i < winOptions.length; i++) { //after 5 plays and every turn thereafter run this loop: 
 				var xPlays = 0;
 				var oPlays = 0;
-					for (var j = 0; j < winOptions[i].length; j++) {
+					for (var j = 0; j < winOptions[i].length; j++) { 
 						for(var k = 0; k < $scope.p1.length; ++k){
 							if($scope.p1[k] == winOptions[i][j]) {
 								if ((++xPlays) == 3)
 								console.log('X wins');
+								//insert inner html XWINS!
+								//push win to scoreboard.
+								//show play again screen.
 								
 							}
 						}
@@ -85,6 +88,9 @@ $scope.p2 = [];
 							if($scope.p2[k] == winOptions[i][j]) {
 								if ((++oPlays) == 3)
 								console.log('O wins');
+								//insert inner html OWINS!
+								//push win to scoreboard.
+								//show play again screen.
 								
 							}
 						}
